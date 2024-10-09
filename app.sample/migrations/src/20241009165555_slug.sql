5efa97a082a25073f63ac19814bfe70f4fca5747
Stéphane Mourey <mail@stephanemourey.fr>
2024-10-09 17:13:43 +0200

    Add slug column to posts

---
+++ app.sample/migrations/src/20241009165555_slug.sql
@@ -0,0 +1,21 @@
+-- migrate:up
+ALTER TABLE private.posts ADD COLUMN slug VARCHAR(255);
+UPDATE private.posts SET slug = lower(replace(replace(replace(title, ' ', '-'), '[', ''), ']', ''));
+ALTER TABLE private.posts ALTER COLUMN slug SET NOT NULL;
+CREATE OR REPLACE FUNCTION set_slug()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.slug := lower(replace(replace(replace(NEW.title, ' ', '-'), '[', ''), ']', ''));
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_slug_trigger
+BEFORE INSERT ON private.posts
+FOR EACH ROW
+EXECUTE PROCEDURE set_slug();
+
+-- migrate:down
+DROP TRIGGER set_slug_trigger ON private.posts;
+DROP FUNCTION set_slug();
+ALTER TABLE private.posts DROP COLUMN slug;
